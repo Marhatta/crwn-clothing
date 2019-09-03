ceb854a07bb6e0d221257f85bdabc05dc598af71
Vishal Marhatta <vishal.marhatta@Eternus.local>
2019-09-03 11:31:55 +0530

    Basic routing setup withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+//Routes set up
+const Root = () => {
+    return(
+    <Router>
+        <App></App>
+    </Router>
+    )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 

403676502f688b5fd30953d5067258da5624f9b6
Vishal Marhatta <vishal.marhatta@Eternus.local>
2019-09-04 11:32:09 +0530

    Added SignUp and stored the authenticated users in firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import {Switch,Route} from 'react-router-dom';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -22,8 +22,24 @@ class App extends Component{
 
   componentDidMount(){
     //onAuthStateChange returns a unSubscribe function
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        //createUserProfileDocument function returns a userRef 
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser:{
+              id:snapshot.id,
+              ...snapshot.data()
+            }
+          });
+          console.log(this.state);      
+        })
+      }
+      else{
+        this.setState({currentUser:userAuth})
+      }
     })
   }
 
